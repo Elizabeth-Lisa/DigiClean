4d1d999eb00344fe1ff9f6c8151c3b6cf398fa70
Elizabeth-Lisa5 <wambuilisa@gmail.com>
2022-01-18 08:50:53 +0300

    add authentication routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,5 +26,31 @@ public class App {
             return new ModelAndView(model, "index.hbs");
 
         }, new HandlebarsTemplateEngine());
+
+        get("/professional/login",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "professional-login.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+        get("/professional/register",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "professional-register.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+        get("/client/login",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "client-login.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+        get("/client/register",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "client-register.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+
     }
 }
